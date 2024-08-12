165b54cfde4d84b7ac900e9a4600dac363a8bd75
Ong Cong Kin <congkinong0112@gmail.com>
2024-08-13 01:15:44 +0800

    fix route error

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return to_route('tasl.index');
+    return to_route('task.index');
 });
 
 Route::resource('task', TaskController::class);    
