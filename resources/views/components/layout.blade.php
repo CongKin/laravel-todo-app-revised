ad698735dc2ff12b03246db0bf78dfc3f371fca0
Ong Cong Kin <congkinong0112@gmail.com>
2024-08-12 23:58:13 +0800

    insert css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,8 +10,16 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body>
+        @session('message')
+            <div class="success-message">
+                {{ session('message') }}
+            </div>
+        @endsession
         {{ $slot }}
     </body>
 </html>
